14a4269e0869a0638ea546718f2922193a78c84f
Kelly Duran <kaduran16@gmail.com>
2022-08-09 12:14:32 -0400

    commit tree 42453bf5a19be3de1c3db31f9277e8a6008f39ac

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,16 @@
+require 'pry'
 class Person
-
+    def name=(name)
+        @name = name 
+    end
+    def name
+        @name 
+    end
+    def job=(job)
+        @job = job 
+    end
+    def job
+        @job 
+    end
 end
+binding.pry
